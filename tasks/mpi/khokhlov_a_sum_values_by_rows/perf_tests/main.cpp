397d3d1321552a10a420b74a750c0f05d5651690
pipidater2004 <chebureknabite@gmail.com>
2025-02-13 11:22:58 +0300

    same values perf tests

--- tasks/mpi/khokhlov_a_sum_values_by_rows/perf_tests/main.cpp
+++ tasks/mpi/khokhlov_a_sum_values_by_rows/perf_tests/main.cpp
@@ -14,8 +14,8 @@
 TEST(khokhlov_a_sum_values_by_rows_mpi, test_pipeline_Run) {
   boost::mpi::communicator world;
 
-  int cols = 15000;
-  int rows = 15000;
+  int cols = 22000;
+  int rows = 22000;
 
   // Create data
   std::vector<int> in(cols * rows, 0);
@@ -75,8 +75,8 @@ TEST(khokhlov_a_sum_values_by_rows_mpi, test_pipeline_Run) {
 TEST(khokhlov_a_sum_values_by_rows_mpi, test_task_Run) {
   boost::mpi::communicator world;
 
-  int cols = 15000;
-  int rows = 15000;
+  int cols = 22000;
+  int rows = 22000;
 
   // Create data
   std::vector<int> in(cols * rows, 0);
